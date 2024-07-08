dd35287eb81ff176fad1a5097f527bdda54a7f2f
esthervankleef <esther.van.kleef@gmail.com>
2024-07-08 16:48:00 +0200

    Clean new implementation data

--- Calculation_weights.R
+++ Calculation_weights.R
@@ -6,21 +6,57 @@
 # Date created: 5 July 2024
 # Date latest update: 
 
+rm(list=ls())
 # Load R packages
 pacman::p_load(readxl, writexl, lubridate, zoo, ggplot2, tidyverse, Hmisc, stringr,lme4,reshape2, 
-               table1, flextable, magrittr, officer)
+               table1, flextable, magrittr, officer, janitor, sf, gtsummary, leaflet)
+
 
 # Locate directories
 dirData = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/GLASS HISTORICAL DATA EV"
+dirDataNew = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/NewDatabaseEV/2022 GLASS data - New DB - for 2024 report"
 dirOutput = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/2024 ANALYSIS EV/2024 Figures_Tables/"
 
-# Load in data
+##############################################################
+# LOAD IN DATA
+##############################################################
+pdata = read.csv(paste0(dirData, "/Final_Curated_Data_GLASS_2023_EV/EI_Popdta_071123 EV.csv"), sep=",")       # Population data
+cdata = read.csv(paste0(dirData, "/Final_Curated_Data_GLASS_2023_EV/EI_Countrydta_071123 EV.csv"), sep=",")   # Country data
+sdata = read.csv(paste0(dirData, "/Final_Curated_Data_GLASS_2023_EV/EI_SurveillanceSites_071123 EV.csv"), sep=",") # Surveillance sites
+idata = read.csv(paste0(dirDataNew,"/Implementation Questionnaire_040724_EV.csv"), sep=";")                   # Implementation data
+idata_old = read.csv(paste0(dirData, "/Final_Curated_Data_GLASS_2023_EV/EI_Implementationdta_071123 EV.csv"), sep=",") # Surveillance sites                   # Implementation data
+
+##############################################################
+# CLEAN DATA
+##############################################################
+
 # Population data
+##############################################################
+
+
+# GLASS Country data
+##############################################################
+
+# GLASS Surveillance sites
 ##############################################################
 
 # GLASS Implementation data
 ##############################################################
 
+# Clean names
+names(idata) = idata[3,]
+names(idata)[1:17] = idata[4,c(1:17)] 
+names(idata)[7:9] = c("NLR_YES","NLR_NO","NLR_UNK")
+names(idata)[10:12] = c("NLR_EQA_YES","NLR_EQA_NO","NLR_EQA_UNK")
+names(idata)[13:17] = c("LAB_EUCAST","LAB_CLSI","LAB_BOTH", "LAB_OTHER", "LAB_UNK")
+
+names(idata) = tolower(names(idata))
+names(idata)[1] = "org_unit"
+names(idata)[4] = "ncc_yes"
+
+names(idata_old)
+names(idata)
+      
 # Universal health coverage data
 ##############################################################
 # Source:
---
+++ Data_wrangling.R
@@ -0,0 +1,99 @@
+#################################################
+# GLASS REPORT - Data wrangling
+#################################################
+
+# Author: Esther van Kleef
+# Date created: 8 July 2024
+# Date latest update: 8 July 2024
+
+rm(list=ls())
+# Load R packages
+pacman::p_load(readxl, writexl, lubridate, zoo, ggplot2, tidyverse, Hmisc, stringr,lme4,reshape2, 
+               table1, flextable, magrittr, officer, janitor, sf, gtsummary, leaflet)
+
+
+# Locate directories
+dirDataOld = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/GLASS HISTORICAL DATA EV"
+dirDataNew = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/NewDatabaseEV/2022 GLASS data - New DB - for 2024 report/Master_Raw_Data_GLASS_2024"
+dirOutput = "C:/Users/Esther/World Health Organization/GLASS Data Visualization - Documents/General/Esther work - GLASS 2024/NewDatabaseEV/2022 GLASS data - New DB - for 2024 report/Final_Curated_Data_GLASS_2024"
+
+##############################################################
+# LOAD IN DATA
+##############################################################
+
+idata = read.csv(paste0(dirDataNew,"/Implementation Questionnaire_040724_EV.csv"), sep=";")                   # Implementation data
+idata_old = read.csv(paste0(dirDataOld, "/Final_Curated_Data_GLASS_2023_EV/EI_Implementationdta_071123 EV.csv"), sep=",") # Implementation data
+
+#############################################################
+# CLEAN DATA
+#############################################################
+
+# Implementation data
+#############################################################
+
+# Clean names
+names(idata) = idata[3,]
+names(idata)[1:17] = idata[4,c(1:17)] 
+names(idata)[7:9] = c("NLR_YES","NLR_NO","NLR_UNK")
+names(idata)[10:12] = c("NLR_EQA_YES","NLR_EQA_NO","NLR_EQA_UNK")
+names(idata)[13:17] = c("LAB_EUCAST","LAB_CLSI","LAB_BOTH", "LAB_OTHER", "LAB_UNK")
+names(idata) = tolower(names(idata))
+
+# Remove first rows
+idata = idata[5:length(idata$period),]
+
+# Make names in line with previous data, so we can reproduce code
+unique(idata_old$Iso3)  # Org unit = Iso3 in historical data
+unique(idata$period)    # Period = Year
+
+idata = idata %>%
+  mutate(AMR_NCC = 
+           case_when(ncc_yes_=="Yes" ~ "Established", 
+                     ncc_no =="Yes"  ~ "Not established",
+                     ncc_in_progress == "Yes"  ~ "Establishment in progress",
+                     ncc_yes_== " " & ncc_no== " " &  ncc_in_progress== " " ~ "Not reported",
+                     TRUE ~ NA),
+         AMR_NLR = 
+            case_when(nlr_yes=="Yes" ~ "Established", 
+                      nlr_no =="Yes"  ~ "Not established",
+                      nlr_unk == "Yes"  ~ "Unknown",
+                      nlr_yes== " " & nlr_no== " " &  nlr_unk== " " ~ "Not reported",
+                      TRUE ~ NA),
+         EQA_to_NRL = 
+           case_when(nlr_eqa_yes=="Yes" ~ "Established", 
+                     nlr_eqa_no =="Yes"  ~ "Not established",
+                     nlr_eqa_unk == "Yes"  ~ "Unknown",
+                     nlr_eqa_yes== " " & nlr_eqa_no== " " &  nlr_eqa_unk== " " ~ "Not reported",
+                     TRUE ~ NA),
+         AMR_AST_standards = 
+           case_when(lab_clsi =="Yes" ~ "CLSI", 
+                     lab_eucast =="Yes"  ~ "EUCAST",
+                     lab_both == "Yes"  ~ "CLSI_EUCAST",
+                     lab_other== "Yes" ~ "Other",
+                     lab_unk == "Yes" ~ "Unknown",
+                     TRUE ~ NA)
+  ) %>%
+  rename(
+    Iso3 = `org unit *`,
+    Year = "period"
+  )
+
+# table(idata$AMR_NCC,idata$ncc_yes_)
+# table(idata$AMR_NCC,idata$ncc_no)
+# table(idata$AMR_NCC,idata$ncc_in_progress)
+# 
+
+# Remove merged variables
+idata = idata %>% select(-c(ncc_yes_, ncc_no, ncc_in_progress, nlr_yes, nlr_no, nlr_unk,
+                            nlr_eqa_yes, nlr_eqa_no, nlr_eqa_unk, lab_clsi,lab_eucast,
+                            lab_both, lab_other, lab_unk))
+names(idata)
+
+# Change characters to numbers
+idata[,c(4:8,10:16)] = sapply(idata[,c(4:8,10:16)], function(x) as.numeric(x))
+
+describe(idata)
+sapply(idata, function(x) class(x))
+
+# Export data
+write.csv(idata, paste0(dirOutput, "/EI_Implementationdta_080724 EV.csv"))
